6e68d388be6bc32467b754e1edeba418010416f8
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 21:58:04 -0300

    Update index.js: add dotenv for environment variables, use TOKEN from env variables
    * Add dotenv for managing environment variables
    * Replace hard-coded token with TOKEN from environment variables
    * Add CLI

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Client, Events, GatewayIntentBits } = require('discord.js')
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// dotenv
+const dotenv = require(`dotenv`)
+dotenv.config()
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Tudo Certo! Logado como ${readyClient.user.tag}`);
-});
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Tudo Certo! Logado como ${readyClient.user.tag}`)
+})
 
 // Log in to Discord with your client's token
-client.login(token);
+client.login(TOKEN)
