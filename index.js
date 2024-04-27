3cf73818b2308d5266621fc206637d4cd93204c6
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 22:34:10 -0300

    Update index.js: use 'dotenv' package, update imports, and correct command handling
    * Update the 'dotenv' package to the latest version and import it correctly
    * Update the 'commands' directory path and the way command

--- index.js
+++ index.js
@@ -2,38 +2,37 @@
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
 // dotenv
-const dotenv = require(`dotenv`)
+const dotenv = require('dotenv')
 dotenv.config()
-
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+const { TOKEN } = process.env
 
 // importação dos comandos
 const fs = require('node:fs')
 const path = require('node:path')
-
-const commandsPath = path.join(__dirname, './src/commands')
+const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(`.js`))
+  .filter((file) => file.endsWith('.js'))
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 client.commands = new Collection()
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath)
+  const filePath = path.join(commandsPath, file)
   const command = require(filePath)
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command)
   } else {
     console.log(
-      `Este comando em ${filePath} está com 'data' ou 'execução' ausentes`,
+      `Esse comando em ${filePath} está com "data" ou "execute ausentes"`,
     )
   }
 }
 
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Tudo Certo! Logado como ${readyClient.user.tag}`)
+// Login do bot
+client.once(Events.ClientReady, (c) => {
+  console.log(`Pronto! Login realizado como ${c.user.tag}`)
 })
-
-// Log in to Discord with your client's token
 client.login(TOKEN)
+
+// Listener de interações com o bot
