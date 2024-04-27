70701a9d0b4a59fbc311787af88f00f9bfc799de
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 22:57:35 -0300

    "Added code to deploy commands to a specific guild using Discord's REST API"
    The diff includes adding a new REST instance with the Discord application's token, setting up the commands array with the command data,

--- deploy-commands.js
+++ deploy-commands.js
@@ -20,3 +20,23 @@ for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   commands.push(command.data.toJSON())
 }
+
+// instância REST
+const rest = new REST({ version: '10' }).setToken(TOKEN)
+
+// deploy
+(async () => {
+    try {
+      console.log(`Resentando ${commands.length} comandos...`)
+
+      // PUT
+      const data = await rest.put(
+        Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+        { body: commands },
+      )
+      console.log('Comandos registrados com sucesso!')
+    } catch (error) {
+      console.error(error)
+    }
+  },
+)()
