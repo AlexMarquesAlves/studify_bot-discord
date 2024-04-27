fbc4d8d0784e3366116831715d604594a931d847
¡Duque <atanaelleonardo@gmail.com>
2024-04-27 01:50:40 -0300

    Update dependencies and imports to Discord.js v14
    This commit updates the dependencies and imports to Discord.js v14, and makes necessary changes to the code to ensure compatibility with the new version. The commit also updates the

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const { REST, Routes } = require("discord.js")
+const { REST, Routes } = require('discord.js')
 
 // dotenv
 const dotenv = require('dotenv')
@@ -6,34 +6,36 @@ dotenv.config()
 const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
 
 // importação dos comandos
-const fs = require("node:fs")
-const path = require("node:path")
-const commandsPath = path.join(__dirname, "commands")
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
+const fs = require('node:fs')
+const path = require('node:path')
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'))
 
 const commands = []
 
-for (const file of commandFiles) { 
-   const command = require(`./commands/${file}`)
-   commands.push(command.data.toJSON())
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
 }
 
 // instância REST
-const rest = new REST({version: "10"}).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN)
 
 // deploy
-(async () => {
-    try {
-        console.log(`Resentando ${commands.length} comandos...`)
-    
-        // PUT
-        const data = await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-            {body: commands}
-        )
-            console.log("Comandos registrados com sucesso!")
-    }
-    catch (error){
-        console.error(error)
-    }
+;(async () => {
+  try {
+    console.log(`Resentando ${commands.length} comandos...`)
+
+    // PUT
+    const data = await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    )
+    console.log('Comandos registrados com sucesso!')
+    return data
+  } catch (error) {
+    console.error(error)
+  }
 })()
