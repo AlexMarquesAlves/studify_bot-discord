b3ac950ad5362a7ac45262c308b475f0c15fe5dd
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 22:53:56 -0300

    Update deploy-commands.js: import required modules, configure dotenv, and read command files
    * Import Discord.js REST and Routes modules
    * Configure dotenv with TOKEN, CLIENT\_ID,

---
+++ deploy-commands.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { REST, Routes } = require('discord.js')
+
+// dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+
+// importação dos comandos
+const fs = require('node:fs')
+const path = require('node:path')
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'))
