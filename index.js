a6f28624c843316f0b019dfbbd10d1a1130af86f
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 22:48:30 -0300

    "Added error throw for missing data or execute properties in command files and added interaction create event listener for string select menu"
    This commit includes a change to the way the bot handles missing properties in command files and the addition of an event listener for

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for (const file of commandFiles) {
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command)
   } else {
-    console.log(
+    throw new Error(
       `Esse comando em ${filePath} está com "data" ou "execute ausentes"`,
     )
   }
@@ -36,3 +36,24 @@ client.once(Events.ClientReady, (c) => {
 client.login(TOKEN)
 
 // Listener de interações com o bot
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isStringSelectMenu()) {
+    const selected = interaction.values[0]
+
+    if (selected === 'javascript') {
+      await interaction.reply(
+        'Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      )
+    } else if (selected === 'python') {
+      await interaction.reply('Documentação do Python: https://www.python.org')
+    } else if (selected === 'csharp') {
+      await interaction.reply(
+        'Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/',
+      )
+    } else if (selected === 'discordjs') {
+      await interaction.reply(
+        'Documentação do Discord.js: https://discordjs.guide/#before-you-begin',
+      )
+    }
+  }
+})
