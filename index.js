4733d2b2a2fa2c7d50db070d5637145c331f64b1
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 22:10:01 -0300

    "Import and register commands in index.js, add ping command in commands/ping.js"
    Explanation:
    
    - The diff in index.js shows the addition of code to import and register commands from the 'src

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const dotenv = require(`dotenv`)
 dotenv.config()
 const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
 
+// importação dos comandos
+const fs = require('node:fs')
+const path = require('node:path')
+
+const commandsPath = path.join(__dirname, 'src/commands')
+const commandsFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(`.{js,ts}`))
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 // When the client is ready, run this code (only once).
