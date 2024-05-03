1a41e5e0cda165f71199bb09a21996e03944c975
¡Duque <atanaelleonardo@gmail.com>
2024-05-02 22:42:45 -0300

    chore add eslint dependencies

--- commands/playlist.js
+++ commands/playlist.js
@@ -7,11 +7,7 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.reply(
-<<<<<<< HEAD
-      'https://open.spotify.com/playlist/1YbHmfZCO8V91Nd0LGoXY2?si=4f850eff20ba4991'
-=======
       'https://open.spotify.com/playlist/5TUxgTIxzLbLVh7RUf9V8i?si=d79ad3b1a72840b6'
->>>>>>> b2d4dd98c86db4014f61c47875589daae1a170d9
     )
   },
 }
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command)
   } else {
     console.log(
-      `Esse comando em ${filePath} está com "data" ou "execute ausentes"`,
+      `Esse comando em ${filePath} está com "data" ou "execute ausentes"`
     )
   }
 }
@@ -41,17 +41,17 @@ client.on(Events.InteractionCreate, async (interaction) => {
     const selected = interaction.values[0]
     if (selected == 'javascript') {
       await interaction.reply(
-        'Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        'Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript'
       )
     } else if (selected == 'python') {
       await interaction.reply('Documentação do Python: https://www.python.org')
     } else if (selected == 'csharp') {
       await interaction.reply(
-        'Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/',
+        'Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/'
       )
     } else if (selected == 'discordjs') {
       await interaction.reply(
-        'Documentação do Discord.js: https://discordjs.guide/#before-you-begin',
+        'Documentação do Discord.js: https://discordjs.guide/#before-you-begin'
       )
     }
   }
